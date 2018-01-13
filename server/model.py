866c12395c92b5e78997f5a7980f3efcc39cdf73
Andranik0 <anakinouki@hotmail.fr>
2018-01-13 12:13:13 +0100

    deploy

--- server/model.py
+++ server/model.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import sys
 # import phe.paillier as he
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from math import isnan, floor
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import cross_val_score
